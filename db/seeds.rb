091c3fb96887bb9444903e90623a16f2427dd97d
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-16 16:43:31 +0200

    almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,30 @@ Flat.create!(
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
-  number_of_guests: 3
+  number_of_guests: 3,
+  image_url: 'https://unsplash.com/photos/jn7uVeCdf6U'
 )
 Flat.create!(
   name: 'Beutiful appartment in the heart of Paris',
   address: '18 rue miollis, 75015 Paris',
   description: 'Beutiful appartment in the heart of Paris: A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 120,
-  number_of_guests: 3
+  number_of_guests: 3,
+  image_url: 'https://unsplash.com/photos/jn7uVeCdf6U'
 )
 Flat.create!(
   name: 'Amazing view in Paris (26th floor)',
   address: '11 rue de Javel',
   description: 'Amazing view in Paris (26th floor): A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 150,
-  number_of_guests: 6
+  number_of_guests: 6,
+  image_url: 'https://unsplash.com/photos/jn7uVeCdf6U'
 )
 Flat.create!(
   name: 'Eiffel tower view!',
   address: '54 rue des Entrepreneurs, 75015 Paris',
   description: '54 rue des Entrepreneurs, 75015 Paris: A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 120,
-  number_of_guests: 4
+  number_of_guests: 4,
+  image_url: 'https://unsplash.com/photos/jn7uVeCdf6U'
 )
