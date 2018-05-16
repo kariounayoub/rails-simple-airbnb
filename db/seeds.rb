8bc7db394fadc13e0f1c2d3a69c2de330fe946e7
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-16 15:04:03 +0200

    migration and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Beutiful appartment in the heart of Paris',
+  address: '18 rue miollis, 75015 Paris',
+  description: 'Beutiful appartment in the heart of Paris: A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Amazing view in Paris (26th floor)',
+  address: '11 rue de Javel',
+  description: 'Amazing view in Paris (26th floor): A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 150,
+  number_of_guests: 6
+)
+Flat.create!(
+  name: 'Eiffel tower view!',
+  address: '54 rue des Entrepreneurs, 75015 Paris',
+  description: '54 rue des Entrepreneurs, 75015 Paris: A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 4
+)
